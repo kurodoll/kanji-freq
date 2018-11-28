f46e4617bc98b00572a480bf485176e0237c96ab
kurodoll <kurodoll@protonmail.ch>
2018-11-28 19:50:42 +1300

    Implement script submission to DB

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,54 @@ router.use(file_upload());
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 // *                                                          * // App Init //
 const website_name = 'Kanji Freak';
+config = require('../config');
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// *                                                           * // DB Init //
+const pg = require('pg');
+const url = require('url');
+
+let db_url;
+let pg_pool;
+
+if (process.env.DATABASE_URL) {
+  db_url = process.env.DATABASE_URL;
+}
+else {
+  db_url = config.db.url;
+}
+
+if (db_url) {
+  const params = url.parse(db_url);
+  const auth = params.auth.split(':');
+
+  const pg_config = {
+    host: params.hostname,
+    port: params.port,
+    user: auth[0],
+    password: auth[1],
+    database: params.pathname.split('/')[1],
+    ssl: true,
+    max: 10,
+    idleTimeoutMillis: 30000 };
+
+  pg_pool = new pg.Pool(pg_config);
+}
+else {
+  console.error('Couldn\'t connect to database as no config could be loaded.');
+}
+
+pg_pool.on('error', function(err, client) {
+  console.error('Idle client error: ', err.message, err.stack);
+});
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// *                                                 * // General Functions //
+function getTimestamp() {
+  return new Date(new Date().getTime()).toISOString();
+}
 
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
@@ -25,8 +73,23 @@ router.post('/upload', function(req, res, next) {
     return res.status(400).send('No files were uploaded.');
   }
   else {
-    // const script_file = req.files.script;
-    res.redirect('/');
+    const script_file = req.files.script;
+
+    const query = 'INSERT INTO scripts (type, title, filename, script, added) VALUES ($1, $2, $3, $4, $5);'; // eslint-disable-line max-len
+    const vars = [
+      req.body.type,
+      req.body.title,
+      script_file.name,
+      script_file.data.toString('utf-8'),
+      getTimestamp() ];
+
+    pg_pool.query(query, vars, (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+
+      res.redirect('/');
+    });
   }
 });
 
