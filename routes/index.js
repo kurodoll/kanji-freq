b0a3a538a0531d03ab5907b4f1786728eb2b9af8
kurodoll <kurodoll@protonmail.ch>
2018-11-28 17:42:03 +1300

    Implement file upload code

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const file_upload = require('express-fileupload');
+router.use(file_upload());
+
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 // *                                                          * // App Init //
@@ -15,6 +18,16 @@ router.get('/', function(req, res, next) {
 
 router.get('/upload', function(req, res, next) {
   res.render('upload', { title: 'Upload a Script - ' + website_name });
+});
+
+router.post('/upload', function(req, res, next) {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  else {
+    // const script_file = req.files.script;
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
